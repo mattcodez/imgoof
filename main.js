2a4ef059ad2cc22a787add3541b79d25d3a4630e
Matthew Molnar <matthew.molnar@gmail.com>
2016-01-24 14:04:59 -0500

    Add a little red

--- main.js
+++ main.js
@@ -3,19 +3,21 @@
 document.addEventListener('DOMContentLoaded',init);
 
 function init(){
-  let canvas = document.querySelector('canvas');
-  let ctx = canvas.getContext('2d');
+  let canvasS = document.querySelector('#start');
+  let canvasF = document.querySelector('#finish');
+  let ctxS = canvasS.getContext('2d');
+  let ctxF = canvasF.getContext('2d');
 
   let img = new Image();
   img.onload = function () {
-    ctx.drawImage(img, 0, 0);
-    imgoof(ctx);
+    ctxS.drawImage(img, 0, 0);
+    imgoof(ctxS, ctxF);
   };
   img.src = 'apple.jpg';
 }
 
-function imgoof(ctx){
-  let imageData = ctx.getImageData(0, 0, 512, 640);
+function imgoof(ctxS, ctxF){
+  let imageData = ctxS.getImageData(0, 0, 512, 640);
   let data = imageData.data;//Uint8ClampedArray
   let buffer = data.buffer;
   let data32 = new Uint32Array(buffer); //Per-pixel access
@@ -23,9 +25,12 @@ function imgoof(ctx){
   let newData = new ArrayBuffer(data.length);
   let newData32 = new Uint32Array(buffer);
   data32.forEach((pixel, i) => {
-    newData32[i] = pixel;
+    let red = pixel & 255;
+    red += 100;
+    red = red > 255 ? 255 : red;
+    newData32[i] = (pixel | red);
   });
 
   data.set(newData32);
-  ctx.putImageData(imageData, 0, 0);
+  ctxF.putImageData(imageData, 0, 0);
 }
