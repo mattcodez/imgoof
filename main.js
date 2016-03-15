547dfdd3395054ea69ba5678aea42174bb080880
Matthew Molnar <matthew.molnar@gmail.com>
2016-03-14 20:58:57 -0400

    Most SIMD logic added. Final output doesn't work, have to use SIMD.store().

--- main.js
+++ main.js
@@ -45,13 +45,50 @@ function imgoof(ctxS, ctxF, colors){
   let newData8 = new Uint8ClampedArray(buffer);
 
   const startRender = new Date();
-  classicLoop(data8, newData8, colors);
-
+  if (typeof SIMD !== 'undefined'){
+    console.log('SIMD Detected!');
+    SIMDLoop(data8, newData8, colors);
+  }
+  else {
+    classicLoop(data8, newData8, colors);
+  }
   const endRender = new Date();
   console.log('Render time: ', endRender - startRender);
 
   data.set(newData8);
   ctxF.putImageData(imageData, 0, 0);
+}
+
+function SIMDLoop(src8, dest8, colors){
+  colors = colors || {};
+  const rMod = colors.red || 0,
+        gMod = colors.green || 0,
+        bMod = colors.blue || 0,
+        aMod = colors.aplha || 0;
+
+  for (let i = 0; i < src8.length; i+=4){
+    let srcSIMD = SIMD.Int8x16(
+      src8[i],  //red
+      src8[i+1],//green
+      src8[i+2],//blue
+      src8[i+3] //alpha
+    );
+
+    let modSIMD = SIMD.Int8x16(
+      rMod,
+      gMod,
+      bMod,
+      aMod
+    )
+
+    //returns Int8x16
+    let final = SIMD.Int8x16.addSaturate(srcSIMD, modSIMD);
+    //FIXME: this doesn't work, will need to use store()
+    dest8[i] = final[0];
+    dest8[i+1] = final[1];
+    dest8[i+2] = final[2];
+    dest8[i+3] = final[3];
+  }
 }
 
 function classicLoop(src8, dest8, colors){
